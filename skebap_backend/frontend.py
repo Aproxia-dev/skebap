58b3b3a65f504d9d021cca50939d9899689ebbce
Octelly <eli@stefek.cz>
2024-02-21 00:17:07 +0100

    bap viewing and auto-view after upload

--- skebap_backend/frontend.py
+++ skebap_backend/frontend.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi.requests import Request
 from fastapi.responses import HTMLResponse
 from jinja2_fragments.fastapi import Jinja2Blocks
 from pathlib import Path
+from .api import read_bap, BapModel
 
 router = APIRouter()
 templates = Jinja2Blocks(directory=str(Path(__file__).resolve().parent / "templates"))
@@ -18,5 +19,23 @@ async def root(request: Request):
     return templates.TemplateResponse(
         "new.html",
         {"request": request, "monaco": True},
+        block_name=block,
+    )
+
+
+@router.get("/bap/{bap_id}", response_class=HTMLResponse)
+async def view(request: Request, bap_id: int):
+    block = None
+
+    if "hx-target" in request.headers:
+        block = request.headers["hx-target"]
+
+    bap = await read_bap(bap_id)
+    if not isinstance(bap, BapModel):
+        raise HTTPException(500, "Invalid Bap data")
+
+    return templates.TemplateResponse(
+        "view.html",
+        {"request": request, "bap_content": bap.content},
         block_name=block,
     )
