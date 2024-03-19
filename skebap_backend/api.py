34b0a007ce1853d64aa854c170a11b45d919b454
Aproxia-dev <apro@r4ilax.eu>
2024-03-19 16:38:52 +0100

    bap data extension

--- skebap_backend/api.py
+++ skebap_backend/api.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from fastapi import HTTPException, APIRouter
 from pydantic import BaseModel
 from .models import Bap
@@ -18,7 +19,12 @@ class BapRequest(BapModel):
 class BapResponse(BapModel):
     id: Optional[int] = None
     content: str
+    creation_time: datetime
+    valid_until: datetime
 
+class UserModel(BaseModel):
+    email: str
+    passHash: str
 
 router = APIRouter()
 
@@ -28,16 +34,30 @@ async def read_bap(bap_id: int) -> BapResponse:
     result = Session(engine).execute(select(Bap).where(Bap.id == bap_id)).first()
     if result == None:
         raise HTTPException(status_code=404, detail="Bap not found")
-    return BapResponse(id=result[0].id, content=result[0].text)
+    return BapResponse(
+		id=result[0].id,
+		content=result[0].text,
+        creation_time=result[0].creation_time,
+        valid_until=result[0].valid_until
+	)
 
 
 @router.post("/")
 async def new_bap(bap: BapRequest) -> BapResponse:
-    new_bap = Bap(text=bap.content)
+    new_bap = Bap(
+        text=bap.content,
+        creation_time=datetime.now(),
+        valid_until=datetime.now() + timedelta(days=14)
+    )
 
     with Session(engine) as session:
         session.add(new_bap)
         session.commit()
         session.refresh(new_bap)
 
-    return BapResponse(id=new_bap.id, content=new_bap.text)
+    return BapResponse(
+        id=new_bap.id,
+        content=new_bap.text,
+        creation_time=new_bap.creation_time,
+        valid_until=new_bap.valid_until
+    )
--- skebap_backend/models.py
+++ skebap_backend/models.py
@@ -1,7 +1,7 @@
-from sqlalchemy import Integer, Text
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
+from datetime import datetime
+from sqlalchemy.types import DateTime, Integer, Text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.schema import ForeignKey
 from .db import engine
 
 
@@ -12,8 +12,17 @@ class Base(DeclarativeBase):
 class Bap(Base):
     __tablename__ = "baps"
 
-    id: Mapped[int] = mapped_column(Integer(), primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
     text: Mapped[str] = mapped_column(Text, nullable=False)
+    author_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=True)
+    creation_time: Mapped[datetime] = mapped_column(DateTime(), nullable=False)
+    valid_until: Mapped[int] = mapped_column(DateTime(), nullable=True)
 
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(Text, nullable=False)
+    pass_hash: Mapped[str] = mapped_column(Text, nullable=False)
 
 Base.metadata.create_all(engine)
